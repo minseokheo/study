c2cfd29b5b4733843768e475a057ebb08bab8ab9
Minseok Heo <tig06015@naver.com>
2023-02-09 00:12:46 +0900

    2023/02/09

--- Python/16194.py
+++ Python/16194.py
@@ -9,5 +9,9 @@ d[1] = price[0]
 for i in range(2, N+1):
   min_price = price[i-1]
 
-  for j in range(i//2):
-    min_price = min(min_price, d[i], d)
+  for j in range(1, i//2+1):
+    min_price = min(min_price, d[i-j]+d[j])
+  
+  d[i] = min_price
+
+print(d[N])
