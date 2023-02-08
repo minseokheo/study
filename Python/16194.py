bcc38a239dbe38b2182629cc2e3c92834e31d3d7
Minseok Heo <tig06015@naver.com>
2023-02-09 00:00:09 +0900

    2023/02/09

---
+++ Python/16194.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+price = list(map(int, input().split()))
+d = [0 for _ in range(N+1)]
+d[1] = price[0]
+
+for i in range(2, N+1):
+  min_price = price[i-1]
+
+  for j in range(i//2):
+    min_price = min(min_price, d[i], d)
