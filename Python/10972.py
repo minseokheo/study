01c559d0ee44dbd86f26e0bedfbe9d6449a0c717
Minseok Heo <tig06015@naver.com>
2023-03-21 22:30:07 +0900

    2023/03/21

---
+++ Python/10972.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+a = list(map(int, input().split()))
+
+for i in range(N-1, 0, -1):
+    if a[i-1] < a[i]:
+        for j in range(N-1, 0, -1):
+            if a[i-1] < a[j]:
+                a[i-1] , a[j] = a[j], a[i-1]
+                a = a[:i] + sorted(a[i:])
+                print(*a)
+                exit()
+
+print(-1)
